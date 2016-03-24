faa7dbc487f8dc0c23e6893634bf6d6fecc44f26
Hiroshi Hongo <aoba2j@gmail.com>
2016-03-24 12:37:00 +0900

    clean up

--- j-league-on-demand.user.js
+++ j-league-on-demand.user.js
@@ -4,7 +4,7 @@
 // @include        https://vod.skyperfectv.co.jp/live_list.php
 // @downloadUrl    https://raw.githubusercontent.com/downloads/aoba/j-league-on-demand/master/j-league-on-demand.user.js
 // @updateUrl      https://raw.githubusercontent.com/downloads/aoba/j-league-on-demand/master/j-league-on-demand.user.js
-// @version        0.4
+// @version        0.5
 // @description    Show J-League On Demand Streaming Video List
 // ==/UserScript==
 (function(){
@@ -17,7 +17,8 @@
 		self.getVideoInfoData = 'episode_type=0&channel_id=65&after_view_flag=1&sort_type=1&order_type=0&pager_range=2&limit=40&page=1';
 		self.LiveUrl = '/episode_detail.php?EI=';
 		self.container = document.getElementById('container');
-		self.newDivId = '____now_on_air';
+		self.liveDivId = '____now_on_air';
+		self.videoDivId = '____video_list';
 		self.now;
 
 		self.insertAfter = function(newNode, referenceNode){
@@ -48,114 +49,83 @@
 			return html.join('');
 		}
 
-		self.createDiv = function(j1, j2, j3, other, title, id){
+		self.createDiv = function(data, title, id){
 			var div = document.createElement('div');
 			if (id){
-				div.setAttribute('id', self.newDivId);
+				div.setAttribute('id', id);
 			}
 			div.setAttribute('class', 'live_contents');
 			div.innerHTML = '<h1 class="pageTitle">' + title + '</h1>';
-			div.innerHTML += self.createHtml(j1, '■J1');
-			div.innerHTML += self.createHtml(j2, '■J2');
-			div.innerHTML += self.createHtml(j3, '■J3');
-			div.innerHTML += self.createHtml(other, '■Other');
+			div.innerHTML += self.createHtml(data.j1, '■J1');
+			div.innerHTML += self.createHtml(data.j2, '■J2');
+			div.innerHTML += self.createHtml(data.j3, '■J3');
+			div.innerHTML += self.createHtml(data.other, '■Other');
 			return div;
 		}
 
-		self.showLiveList = function(){
+		self.classify = function(episodes, isLive){
+			isLive = (isLive) ? true : false;
+			var highlightRegexp = /ハイライト/;
+			var data = {'j1':[],'j2':[],'j3':[],'other':[]};
+			for(var i in episodes){
+				var epi = episodes[i];
+				var name = epi.episode_name;
+				if (isLive){
+					// isLive=true の場合、現在放送中の映像ソースのみが対象
+					if (self.now < epi.encode_start_end){
+						continue;
+					}
+				} else {
+					// isLive=false の場合、ハイライトはotherへ追加
+					if (name.match(highlightRegexp)){
+						data.other.push(epi);
+						continue;
+					}
+				}
+				if (0 < name.indexOf('J1')){
+					data.j1.push(epi);
+				} else if (0 < name.indexOf('J2')){
+					data.j2.push(epi);
+				} else if (0 < name.indexOf('J3')){
+					data.j3.push(epi);
+				} else {
+					data.other.push(epi);
+				}
+			}
+			return data;
+		}
+
+		self.showLiveList = function(postData, title, isLive){
 			self.now = self.getYYYYMMDDHHMISS();
+			isLive = (isLive) ? true : false;
+			newDivId = (isLive) ? self.liveDivId : self.videoDivId;
 			GM_xmlhttpRequest({
 				method: "POST",
-				url: self.getLiveInfoUrl,
-				data: self.getLiveInfoData,
+				url: self.getLiveInfoUrl + '?cache=' + Date.now(),
+				data: postData,
 				headers: {
 					"Content-Type": "application/x-www-form-urlencoded"
-					,"Accept": "application/x-www-form-urlencoded"
+					,"Accept": "application/json, text/javascript, */*; q=0.01"
+					,"Referer": "https://vod.skyperfectv.co.jp/live_list.php"
+					,"X-Requested-With": "XMLHttpRequest"
 				},
 				onload: function(response){
 					var json = JSON.parse(response.responseText);
 					var episodes = json[1].episode;
-					var j1 = [];
-					var j2 = [];
-					var j3 = [];
-					var other = [];
-					
-					for(var i in episodes){
-						var epi = episodes[i];
-						// 現在放送中の映像ソースのみが対象
-						if (self.now < epi.encode_start_end){
-							continue;
-						}
-						var name = epi.episode_name;
-						if (0 < name.indexOf('J1')){
-							j1.push(epi);
-						} else if (0 < name.indexOf('J2')){
-							j2.push(epi);
-						} else if (0 < name.indexOf('J3')){
-							j3.push(epi);
-						} else {
-							other.push(epi);
-						}
+					var data = self.classify(episodes, isLive);
+					var div = self.createDiv(data, title, newDivId);
+					if (isLive){
+						self.container.insertBefore(div, self.container.firstChild);
+						self.showLiveList(self.getVideoInfoData, 'Video List', false);
+					} else {
+						self.insertAfter(div, document.getElementById(self.liveDivId));
 					}
-					var div = self.createDiv(j1, j2, j3, other, 'Now On Air!', self.newDivId);
-					self.container.insertBefore(div, self.container.firstChild);
-					self.showVideoList();
 				}
 			});
 		}
 
-		self.showVideoList = function(){
-			GM_xmlhttpRequest({
-				method: "POST",
-				url: self.getLiveInfoUrl,
-				data: self.getVideoInfoData,
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded"
-					,"Accept": "application/x-www-form-urlencoded"
-				},
-				onload: function(response){
-					var json = JSON.parse(response.responseText);
-					var episodes = json[1].episode;
-					var regexp = /第([0-9]+)節/; // 第3節 川崎F VS 名古屋
-					var highlightRegexp = /ハイライト/;
-					var j1 = [];
-					var j2 = [];
-					var j3 = [];
-					var other = [];
-					
-					for(var i in episodes){
-						var epi = episodes[i];
-						if (self.now < epi.encode_end_end){
-							continue;
-						}
-						var name = epi.episode_name;
-						// ハイライトはotherへ追加
-						if (name.match(highlightRegexp)){
-							other.push(epi);
-							continue;
-						}
-						// 「第n節」が含まれる場合は試合とみなす
-						if (name.match(regexp)){
-							if (0 < name.indexOf('J1')){
-								j1.push(epi);
-							} else if (0 < name.indexOf('J2')){
-								j2.push(epi);
-							} else if (0 < name.indexOf('J3')){
-								j3.push(epi);
-							} else {
-								other.push(epi);
-							}
-						} else {
-							other.push(epi);
-						}
-					}
-					var div = self.createDiv(j1, j2, j3, other, 'Video List');
-					self.insertAfter(div, document.getElementById(self.newDivId));
-				}
-			});
-		}
 	}
 
 	var j = new JOD();
-	j.showLiveList();
+	j.showLiveList(j.getLiveInfoData, 'Now On Air!', true);
 })();
